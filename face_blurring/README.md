dfd2c980b49f2d552619e9abdc7698fac0bc0f33
OfficerChul <gcj1234567890@gmail.com>
2023-04-21 02:32:39 -0400

    .

--- face_blurring/README.md
+++ face_blurring/README.md
@@ -6,6 +6,34 @@
     - Failed to use GPU of Retinaface due to the dependency issues
         - Driver updates automatically
 - Should try multiple ways of face blurring method to use GPU
+- Differences between Deepface and Retinaface
+    - DeepFace is better suited for large-scale facial recognition tasks, while RetinaFace is ideal for detecting and analyzing faces in complex and dynamic environments.
 ```
 
 ## Method 1: Using Haar-cascade algorithm in OpenCV
+```
+Haar-cascade algorithm couldn't detect the face in the video
+```
+
+## Method 2: Using Mediapipe Face Detection Algorithm
+```angular2html
+It cannot detect the face.
+```
+
+## Method 3: Using DeepFace()
+DeepFace is the most lightweight face recognition and facial attribute analysis library for Python.
+```
+
+```
+
+## Method 4: Using Retinaface
+RetinaFace is a deep learning based cutting-edge facial detector for Python coming with facial landmarks.
+```angular2html
+Reference: https://github.com/elliottzheng/face-detection
+```
+landmarks in Retinaface
+- right_eye
+- left_eye
+- nose
+- mouth_right
+- mouth_left
--- face_blurring/face_blur.py
+++ face_blurring/face_blur.py
@@ -1,44 +1,21 @@
-try:
-    import cv2
-except ImportError:
-    print('opencv is not imported.')
+from skimage import data, io, filters
+from skimage.filters import gaussian
+from retinaface import RetinaFace
 import numpy as np
-import os
 import matplotlib.pyplot as plt
 
-print(os.getcwd())
-xml = 'haarcascade_frontalface_default.xml'
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + xml)
+import cv2
 
-for idx, vid in enumerate(os.listdir('./example_vids')):
-    print(f'{idx} vid: {vid}')
-    vid_path = os.path.join('example_vids', vid)
-    cap = cv2.VideoCapture(os.path.join(os.getcwd(), vid_path))
+import torch
+img = cv2.imread('img.jpg')
+faces = RetinaFace.detect_faces('img.jpg')
+for face in faces.keys():
+    face = faces[face]
+    facial_area = face['facial_area']
 
-    while True:
-        ret, frame = cap.read()
-        frame = cv2.flip(frame, 1)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.05, 5)
+    cv2.rectangle(img, (facial_area[2], facial_area[3]), (facial_area[0], facial_area[1]), (255, 255, 255), 10)
 
-        print(f'number of face detected: {len(faces)}')
+plt.imshow(img[:, :, ::-1])
+plt.show()
 
-        if len(faces):
-            for (x, y, w, h) in faces:
-                # face_img = frame[y:y+h, x:x+w]
-                #
-                # face_img = cv2.resize(face_img, dsize=(0,0), fx=0.04, fy=0.04)
-                # face_img = cv2.resize(face_img, (w, h), interpolation=cv2.INTER_AREA)
-                # frame[y:y+h, x:x+w] = face_img
 
-                cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                roi = gray[y:y + h, x:x + w]
-                # applying a gaussian blur over this new rectangle area
-                roi = cv2.GaussianBlur(roi, (23, 23), 30)
-                # impose this blurred image on original image to get final image
-                gray[y:y + roi.shape[0], x:x + roi.shape[1]] = roi
-
-        plt.imshow(gray, cmap="gray")
-        plt.axis('off')
-        plt.style.use('seaborn')
-        plt.show()
