28ab2acf600ebe94905856be270e8bb213438996
OfficerChul <gcj1234567890@gmail.com>
2023-04-21 16:04:44 -0400

    .

--- face_blurring/face_blur.py
+++ face_blurring/face_blur.py
@@ -1,21 +1,43 @@
 from skimage import data, io, filters
-from skimage.filters import gaussian
 from retinaface import RetinaFace
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import timedelta
 
 import cv2
+import time
 
-import torch
-img = cv2.imread('img.jpg')
-faces = RetinaFace.detect_faces('img.jpg')
-for face in faces.keys():
-    face = faces[face]
-    facial_area = face['facial_area']
+tic = time.time()
 
-    cv2.rectangle(img, (facial_area[2], facial_area[3]), (facial_area[0], facial_area[1]), (255, 255, 255), 10)
+cap = cv2.VideoCapture('./example_vid/20200901_120617.MOV')
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+fps = cap.get(cv2.CAP_PROP_FPS)
+out = cv2.VideoWriter('output_video.avi', fourcc, 30, (1920, 1080))
 
-plt.imshow(img[:, :, ::-1])
-plt.show()
+total = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+print(f'total frame: {total}')
 
+
+count = 0
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    count += 1
+    print(f'count: {count}/{total}')
+    frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    faces = RetinaFace.detect_faces(frame)
+    for face in faces.keys():
+        face = faces[face]
+        facial_area = face['facial_area']
+
+        cv2.rectangle(frame, (facial_area[2], facial_area[3]), (facial_area[0], facial_area[1]), (255, 255, 255), 10)
+
+    out.write(frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+elapsed = time.time() - tic
+formatted_elapsed = str(timedelta(seconds=elapsed))
+print(f'Elapsed Time(%hh:%mm:%ss.xxx): {formatted_elapsed}')
 
